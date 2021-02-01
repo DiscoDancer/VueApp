468a10b201d83eb43aabf116c3a053e9bfcfcdfa
Vladislav Evtushenko <Vladislav.Evtushenko@euromoneyplc.com>
2021-02-01 10:25:47 +0300

    2 pages with navigation

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,8 +1,77 @@
 <template>
-  <v-app>
-    <h1>Hello world!</h1>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Demo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Vue app
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      color="primary"
+      dark
+      src="purple.jpg"
+      app
+      prominent
+      height="170"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-container class="pa-0 ma-0">
+        <v-row>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-spacer></v-spacer>
+          <h1>live search</h1>
+        </v-row>
+        <v-row>
+          <v-app-bar-title class="ml-4 text-h4">Vue app</v-app-bar-title>
+        </v-row>
+        <v-row>
+          <h1 class="ml-4">live time</h1>
+        </v-row>
+      </v-container>
+
+    </v-app-bar>
     <v-main>
+      <!--  -->
       <router-view></router-view>
+      <!--      <snackbar/>-->
     </v-main>
   </v-app>
 </template>
@@ -11,7 +80,11 @@
 export default {
   name: 'App',
   data: () => ({
-    //
+    drawer: null,
+    items: [
+      { title: 'Browse', icon: 'mdi-format-list-checks', to: '/' },
+      { title: 'Create', icon: 'mdi-plus', to: '/create' },
+    ],
   }),
 };
 </script>
