2cdc56ec4b9b9a3ac09e0aef7cd33c660cc7cec2
Vladislav Evtushenko <Vladislav.Evtushenko@euromoneyplc.com>
2021-02-01 10:14:31 +0300

    added 2 empty views

---
+++ my-app/src/views/Browse.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello world from Browse</h1>
+</template>
+
+<script>
+export default {
+  name: 'Browse',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ my-app/src/views/Create.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello world from Create</h1>
+</template>
+
+<script>
+export default {
+  name: 'Create',
+};
+</script>
+
+<style scoped>
+
+</style>
